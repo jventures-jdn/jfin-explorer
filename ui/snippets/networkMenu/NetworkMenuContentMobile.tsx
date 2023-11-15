d03b486ef4dc32a810644b278b582b4dd0360653
bestsilly <anuwut.kon@gmail.com>
2023-11-15 10:38:02 +0700

    feat: Add dynamic menu handling on mobile view

--- ui/snippets/networkMenu/NetworkMenuContentMobile.tsx
+++ ui/snippets/networkMenu/NetworkMenuContentMobile.tsx
@@ -1,7 +1,11 @@
-import { Box, VStack, Skeleton, Flex } from '@chakra-ui/react';
-import React from 'react';
+/* eslint-disable react/jsx-no-bind */
+import { Box, VStack, Skeleton, Flex, useColorModeValue, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
 
 import type { NetworkGroup, FeaturedNetwork } from 'types/networks';
+
+import chevronIcon from 'icons/arrows/east-mini.svg';
+import Icon from 'ui/shared/chakra/Icon';
 
 import NetworkMenuLink from './NetworkMenuLink';
 
@@ -12,6 +16,19 @@ interface Props {
 
 const NetworkMenuContentMobile = ({ items }: Props) => {
   {/* JFIN Mod Start */}
+  const [ selectedMenu, setSelectedMenu ] = useState<string | undefined>();
+  const iconColor = useColorModeValue('purple.600', 'purple.200');
+
+  const handleClickMenu = (network: FeaturedNetwork) => {
+    if (!network?.subMenu) {
+      return;
+    }
+    setSelectedMenu(network.title);
+  };
+
+  const handleClickBack = () => {
+    setSelectedMenu(undefined);
+  };
   {/* JFIN Mod End */}
 
   const content = !items || items.length === 0 ? (
@@ -35,17 +52,45 @@ const NetworkMenuContentMobile = ({ items }: Props) => {
   ) : (
     <>
       { /* JFIN Mod Start */ }
-      <VStack as="ul" spacing={ 2 } alignItems="stretch">
-        { items
-          .map((network) => (
-            <NetworkMenuLink
-              key={ network.title }
-              { ...network }
-              isMobile
-            />
-          ))
-        }
-      </VStack>
+      { selectedMenu ? (
+        <>
+          {
+            selectedMenu && (
+              <Flex alignItems="center" onClick={ handleClickBack }>
+                <Icon as={ chevronIcon } boxSize={ 6 } mr={ 2 } color={ iconColor } cursor="pointer"/>
+                <Text as="h4" fontSize="18px" lineHeight="30px" fontWeight="500">{ selectedMenu }</Text>
+              </Flex>
+            )
+          }
+          <VStack as="ul" spacing={ 2 } mt={ 4 } alignItems="stretch">
+            { items.find((item) => item.title === selectedMenu)?.subMenu?.map(subMenu => (
+              <NetworkMenuLink
+                group="Mainnets"
+                key={ subMenu.title }
+                isMobile
+                { ...subMenu }/>
+            ),
+            ) }
+          </VStack>
+        </>
+      ) : (
+        <VStack as="ul" spacing={ 2 } alignItems="stretch">
+          { items
+            .map((network) => (
+              <Box
+                key={ network.title }
+                onClick={ () => handleClickMenu(network) }
+              >
+                <NetworkMenuLink
+                  key={ network.title }
+                  { ...network }
+                  isMobile
+                />
+              </Box>
+            ))
+          }
+        </VStack>
+      ) }
       { /* JFIN Mod End */ }
     </>
   );
