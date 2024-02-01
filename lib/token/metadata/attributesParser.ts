403e03da1bcaccff25dc3da95fa3cc8108b4a289
bestsilly <anuwut.kon@gmail.com>
2024-02-01 12:15:37 +0700

    fix: Enhance date attribute handling in parser

--- lib/token/metadata/attributesParser.ts
+++ lib/token/metadata/attributesParser.ts
@@ -17,11 +17,25 @@ function formatValue(value: string | number, display: string | undefined, trait:
         value: `${ value }% boost`,
       };
     }
+
+    // JFIN Start Mod
     case 'date': {
+      const LOWER_LIMIT = 1e12; // 1 trillion, a common range for milliseconds
+      const UPPER_LIMIT = 1e15; // 1 quadrillion, a common range for seconds
+      const inputDate = Number(value);
+      let dateResult;
+      if (inputDate >= LOWER_LIMIT && inputDate <= UPPER_LIMIT) {
+        // Assuming it's milliseconds
+        dateResult = inputDate;
+      } else {
+        // Assuming it's in seconds, convert it to milliseconds
+        dateResult = inputDate * 1000;
+      }
       return {
-        value: dayjs(value).format('YYYY-MM-DD'),
+        value: dayjs(dateResult).format('YYYY-MM-DD'),
       };
     }
+    // JFIN End Mod
     default: {
       try {
         if (trait?.toLowerCase().includes('url') || value.toString().startsWith('http')) {
